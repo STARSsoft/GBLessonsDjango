24f41e122d75473b1fc15ab477915b103c232a00
STARS <kazssr@gmail.com>
2024-09-16 20:43:06 +0500

    Добавил несколько файлов для приложения Shopapp

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,8 +1,21 @@
+from timeit import default_timer
+
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 def shop_index(request: HttpRequest):
-    print(request.path)
-    print(request.method)
-    print(request.headers)
-    return HttpResponse("<H1>Hello World!</H1>")
+    # print(request.path)
+    # print(request.method)
+    # print(request.headers)
+    # return HttpResponse("<H1>Hello World!</H1>")
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
